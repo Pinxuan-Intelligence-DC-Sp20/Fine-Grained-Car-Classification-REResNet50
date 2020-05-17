781128eb1005b9ea42baebc61f4fadabec7ef3f8
stefan <stef-ljt@berkeley.edu>
2020-05-16 17:28:23 -0700

    Stef's model in py

--- SEResNet2.py
+++ SEResNet2.py
@@ -22,6 +22,9 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint,ReduceLROnPlateau,EarlyStopping,TensorBoard
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 # dict with 'make_names' and 'model_names' as keys
 mat = loadmat('car_type/make_model_name.mat')
@@ -43,11 +46,14 @@ datagen = ImageDataGenerator(
 
 
 
+# TODO: adjust hyperparameter num_make to the number of car makes you would like to include in training
+# num_make should be an integer between 1 and 163
+num_make = 10
 # cancatnating make, model and year into classes
 path = 'CompCars_DataSet/data/images'
 classes = []
 for make in os.listdir(path):
-  if int(make) <= 10:
+  if int(make) <= num_make:
     make_path = path + '/' + make
     for model in os.listdir(make_path):
       model_path = make_path + '/' + model
@@ -57,9 +63,9 @@ for make in os.listdir(path):
           classes.append(class_name)
 num_classes = len(classes)
 
-# %%
+# images
 data = []
-path = '/content/drive/My Drive/fine_grained_car_classification/CompCars_DataSet/data/images'
+path = 'CompCars_DataSet/data/images'
 for make in os.listdir(path):
   if int(make) <= 10:
     make_path = path + '/' + make
@@ -70,17 +76,19 @@ for make in os.listdir(path):
         for image in os.listdir(year_path):
           im = cv2.imread(year_path + '/' + image)
           data.append((im, classes.index(str(model) + '_' + str(year))))
-
 num_data = len(data)
-num_data
 
-# %%
+
+# we did not choose to import sklearn.train_test_split because using all images might overload runtime
+# TODO: adjust m, n--the number of training/testing imagese included, respectively
+m = 300
+n = 100
 random.shuffle(data)
-data_train = data[0:300]
-data_test = data[100:-1]
-len(data_train)
+data_train = data[0:m]
+data_test = data[n:-1]
+print(m + n <= num_data) # this should be true
 
-# %%
+# generate training images
 def gen_train(d, max_size_length, batch_size=6):
     while 1:
         y = []
@@ -117,7 +125,7 @@ def gen_train(d, max_size_length, batch_size=6):
         y = to_categorical(y, num_classes)
         yield x, y
 
-# %%
+# generate testing images
 def gen_test(d, max_size_length, batch_size=6):
     while 1:
         y = []
@@ -154,16 +162,23 @@ def gen_test(d, max_size_length, batch_size=6):
         y = to_categorical(y, num_classes)
         yield x, y
 
-# %%
+
+# training prep
+# TODO: Adjust these hyperparameters
+train_count = 4         # the number of attempts you've tried to tune your model. 
+						# This will correspond to your model's saved name in "models/"
+learning_rate = 0.0006  # learning rate fed into Adam Optimizer
+batch_size = 8
+image_dimension = 256   # training/testing images will be (image_dimension * image_dimension)px^2
+
 model = SEResNet50(input_shape=(None,None,3), weights=None, include_top=True,classes=num_classes)
-adam = Adam(lr=0.0006)
+adam = Adam(lr=learning_rate)
 model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
 
-batch_size = 8
 val_step = ceil(len(data_train) / (2 * batch_size))
 print(val_step)
-train_loader = gen_train(data_train, 256, batch_size=batch_size)
-test_loader = gen_test(data_train, 256, batch_size=batch_size)
+train_loader = gen_train(data_train, image_dimension, batch_size=batch_size)
+test_loader = gen_test(data_train, image_dimension, batch_size=batch_size)
 
 checkpoint = ModelCheckpoint(filepath='./models/weights-senet-{epoch:02d}-{val_loss:.2f}.h5',
                              monitor='val_loss', save_best_only=True)
@@ -172,6 +187,7 @@ reduce_lr = ReduceLROnPlateau(monitor='val_loss', patience=4, mode='auto')
 tensorboard = TensorBoard(log_dir='./models/logs', write_graph=True)
 
 
+# training
 print('-----------Start training-----------')
 history = model.fit_generator(train_loader,
     steps_per_epoch = 50,
@@ -182,17 +198,14 @@ history = model.fit_generator(train_loader,
     validation_steps = val_step,
     callbacks = [checkpoint, reduce_lr, tensorboard])
 
-model.save('/content/drive/My Drive/fine_grained_car_classification/car_type/trial4.h5')
+model.save('models/trial' + str(train_count) + ".h5")
 
-# %%
+
+# loss visualization
 acc_dict = history.history
 acc_dict.keys()
 trial4 = pd.DataFrame(acc_dict)
 trial4.head()
-
-# %%
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 
 fig, ax = plt.subplots(figsize=(10, 10))
@@ -205,4 +218,3 @@ plt.ylabel('Training Stats')
 plt.title('Training Progress')
 
 
-# %%
